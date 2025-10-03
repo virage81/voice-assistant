4222d54023b1456421fd04b062d66ca01f260cc8
virage81 <karimovminds@gmail.com>
2025-10-03 19:54:22 +0400

    chore: init an empty project

--- src/lib/nylas/index.ts
+++
@@ -1,4 +0,0 @@
-import { config } from '@/config';
-import Nylas from 'nylas';
-
-export const nylas = new Nylas({ apiKey: config.nylas.apiKey, apiUri: config.nylas.apiUri });
--- src/lib/vapi/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '@/config';
-import Vapi from '@vapi-ai/web';
-
-export * from './assistant';
-export * from './types';
-
-const vapi = new Vapi(config.vapi.apiKey);
-
-export default vapi;
